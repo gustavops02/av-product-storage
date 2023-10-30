14d07a812d050e2cbcde872f597f554de0be299a
Gustavo Paulo <gustavo.psilvaa02@gmail.com>
2023-10-30 17:05:01 -0300

    [feat] implementação principal do index.py

---
+++ src/index.py
@@ -0,0 +1,9 @@
+from database import crud
+import tkinter as tk
+
+janela = tk.Tk()
+# principal = PrincipalBD(janela)
+
+janela.title('Bem vindo a Tela de Cadastro')
+janela.geometry('600x500')
+janela.mainloop()

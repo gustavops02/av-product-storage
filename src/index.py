a6faf00db85e905da075e4cbe836ace9a1205c05
Gustavo Paulo <gustavo.psilvaa02@gmail.com>
2023-11-01 12:03:58 -0300

    delete all

--- src/index.py
+++
@@ -1,9 +0,0 @@
-from database import crud
-import tkinter as tk
-
-janela = tk.Tk()
-# principal = PrincipalBD(janela)
-
-janela.title('Bem vindo a Tela de Cadastro')
-janela.geometry('600x500')
-janela.mainloop()

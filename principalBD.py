50ae10995eefeffb7a1acaffa3f3bfd880906876
Gustavo Paulo <gustavo.psilvaa02@gmail.com>
2023-11-08 12:50:04 -0300

    [feat] criação da table para mostrar os registros do banco

--- principalBD.py
+++ principalBD.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import ttk
 import crud as crud
 
 class PrincipalBD:
@@ -17,6 +18,8 @@ class PrincipalBD:
         self.btnAtualizar=tk.Button(win, text='Atualizar', command=self.fAtualizarProduto)
         self.btnExcluir=tk.Button(win, text='Excluir', command=self.fExcluirProduto)
         self.btnLimpar=tk.Button(win, text='Limpar', command=self.fLimparTela)
+        self.btnConsultar=tk.Button(win, text='Consultar')
+        self.btnConsultar.bind("<Button>", lambda e: self.consultaProdutos(win)) # cria uma nova tela
         self.lbCodigo.place(x=100, y=50)
         self.txtCodigo.place(x=250, y=50)
         self.lblNome.place(x=100, y=100)
@@ -27,6 +30,30 @@ class PrincipalBD:
         self.btnAtualizar.place(x=200, y=200)
         self.btnExcluir.place(x=300, y=200)
         self.btnLimpar.place(x=400, y=200)
+        self.btnConsultar.place(x=250, y= 250)
+    
+    def consultaProdutos(self, master):
+        novaJanela = tk.Toplevel(master)
+        novaJanela.title('Consulta de produtos')
+        novaJanela.geometry('800x600')
+        tree = ttk.Treeview(novaJanela, selectmode='browse', columns=("col1","col2", "col3", "col4"), show='headings')
+        tree.column("col1", width=100, minwidth=50, stretch='no')
+        tree.heading("#1", text="Id")
+        tree.column("col2", width=200, minwidth=50, stretch='no')
+        tree.heading("#2", text="Nome")
+        tree.column("col3", width=200, minwidth=50, stretch='no')
+        tree.heading("#3", text="Preço")       
+        tree.column("col4", width=300, minwidth=50, stretch='no')
+        tree.heading("#4", text="Preço Ajustado") 
+        tree.pack(expand=True, fill='both') 
+        try:
+            record = self.objBD.getDados()
+            for row in record:
+                tree.insert('', 'end', values=row)
+                
+
+        except(Exception) as error:
+            print(error)
     
     def fCadastrarProduto(self):
         try:
